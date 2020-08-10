1f259ec36ea70358e4e1c6bed5e8372bddb5ab37
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-08-11 03:53:20 +0530

    Login page integrated with dashboard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import NavBar from './components/NavBar';
 import SearchBox from './components/SearchBox';
 function App() {
+  const [count, setCount] = React.useState(0);
+  const update = () => {
+    setCount(1);
+  }
   return (
     <div className="App">
-      <Login/>
-      {/* <h3>Polynomial equation Calculator</h3>
-      <br/>
-      <br/>
-      <SearchBox />
-      <NavBar /> */}
+      {count === 0 &&
+        <Login change={update} />
+      }
+      {count === 1 &&
+        <>
+          <h3>Polynomial equation Calculator</h3>
+          <br />
+          <br />
+          <SearchBox />
+          <NavBar />
+        </>}
     </div>
   );
 }
--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,12 +1,15 @@
 import React from "react";
-const Login = () => {
+const Login = (props) => {
+  const update=()=>{
+    props.change();
+  }
   return (
     <>
       <h1>Login Page</h1>
       <form>
         <p>User Name: <input type="text" name="username" /></p>
         <p>Password: <input type="password" name="userpassword" /></p>
-        <p><input type="Submit" value="Login" /></p>
+        <p><input type="Submit" value="Login" onClick={update} /></p>
       </form>
     </>);
 }
